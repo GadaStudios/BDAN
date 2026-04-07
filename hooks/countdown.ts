bbdce4669318aa30292aefbe80cccba9382c78a1
“thelastofinusa” <thelastofinusa@gmail.com>
2026-04-07 23:05:36 +0100

    refactor: consolidate countdown utilities into utils module

--- hooks/countdown.ts
+++ hooks/countdown.ts
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { getCountdown, Countdown } from "@/lib/countdown"
+import { getCountdown, Countdown } from "@/lib/utils"
 
 export function useCountdown(targetDate: Date) {
   const [time, setTime] = useState<Countdown>(getCountdown(targetDate))
